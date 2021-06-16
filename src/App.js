3283e9dc77f2f169d8f0b6c45ba1852e1b898fe0
Omar <omar0940@gmail.com>
2021-06-16 14:54:41 +0200

    Design shapes & create main

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Design from "./components/Design";
+import Main from "./components/Main";
 import Header from "./components/Header";
 import Myproject from "./components/Myproject";
 import Aboutme from "./components/Aboutme";
@@ -9,6 +10,7 @@ function App() {
     <React.Fragment>
       <Header />
       <Design />
+      <Main />
 
       <Myproject />
       <Aboutme />
---
+++ src/components/Main.js
@@ -0,0 +1,10 @@
+const Main = () => {
+  return (
+    <div className="herosection">
+      <p>
+        Hello, I'm <span>OMAR KHOUJA</span> Junior Web Developer
+      </p>
+    </div>
+  );
+};
+export default Main;
