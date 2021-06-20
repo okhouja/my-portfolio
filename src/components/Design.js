52f21183171ec978461510f1c891346a27c8964d
Omar <omar0940@gmail.com>
2021-06-20 17:16:08 +0200

    redisgn shape 3 & footer

--- src/components/Design.js
+++ src/components/Design.js
@@ -4,7 +4,9 @@ const Design = () => {
       <main>
         <div className="shape1"></div>
         <div className="shape2"></div>
-        <div className="shape3"></div>
+        <div className="shape3__container">
+          <div className="shape3"></div>
+        </div>
       </main>
       {/* <div class="area">
         <ul class="circles">
