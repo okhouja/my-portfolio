238a39fb3833c167d30543672c0f1bf6c4f01df0
Omar <omar0940@gmail.com>
2021-06-21 20:28:42 +0200

    about me section

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -6,19 +6,17 @@ const Aboutme = () => {
   return (
     <React.Fragment>
       <div className="aboutme__Container">
-        <div className="aboutme__title">
-          About Me
-          <div className="aboutme__Header">
-            My name is Omar Khouja. I'm {myyear} Years Old, and Live in Hamburg
-          </div>
-          <div className="aboutme__Main">
-            I came from Aleppo - Syria, I live in Hamburg - Germany since 2015.
-            I studied two years at the Technical Institute of Electricity and
-            Mechanics in Aleppo - Communications Department. I worked in IT
-            Department most of the Time and my dream always learning Web
-            Development and now I learning Web Development at Digital Career
-            Institute School.
-          </div>
+        <div className="aboutme__title">About Me</div>
+        <div className="aboutme__Header">
+          My name is Omar Khouja. I'm {myyear} Years Old, and Live in Hamburg
+        </div>
+        <div className="aboutme__Main">
+          I came from Aleppo - Syria, I live in Hamburg - Germany since 2015. I
+          studied two years at the Technical Institute of Electricity and
+          Mechanics in Aleppo - Communications Department. I worked in IT
+          Department most of the Time and my dream always learning Web
+          Development and now I learning Web Development at Digital Career
+          Institute School.
         </div>
       </div>
     </React.Fragment>
