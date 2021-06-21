3f854083e6fad53a838848a92419fbb2ae4c7fac
Omar <omar0940@gmail.com>
2021-06-21 21:14:03 +0200

    contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,39 @@ import React from "react";
 const Contact = () => {
   return (
     <React.Fragment>
-      <div>Contact Me :)</div>;
+      <div>
+        <article class="contact__container" id="contact">
+          <div class="contact__title">
+            <h2>Contact Me :)</h2>
+          </div>
+
+          <form action="#">
+            <div class="content__form">
+              <fieldset class="input__fields">
+                <input type="text" class="input" placeholder="Name" />
+                <input
+                  type="text"
+                  class="input"
+                  placeholder="Email Address"
+                  required
+                />
+                <input type="text" class="input" placeholder="Phone" />
+                <textarea
+                  type="text"
+                  class="input"
+                  placeholder="Your Message :"
+                ></textarea>
+                <input
+                  type="submit"
+                  class="button button__submit"
+                  value="Submit"
+                />
+              </fieldset>
+            </div>
+          </form>
+        </article>
+      </div>
+      ;
     </React.Fragment>
   );
 };
